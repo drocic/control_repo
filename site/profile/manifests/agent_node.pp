81aaecf50d4948ed15b23f888f4e0bea19d3b6b9
drocic <59014769+drocic@users.noreply.github.com>
2021-04-12 23:36:53 +0200

    Update agent_node.pp

--- site/profile/manifests/agent_node.pp
+++ site/profile/manifests/agent_node.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node{'localhost.localdomain':}  
-  dockeragent::node{'db.localdomain':}
+  dockeragent::node{'web.puppet.vm':}  
+  dockeragent::node{'db.puppet.vm':}
 }
