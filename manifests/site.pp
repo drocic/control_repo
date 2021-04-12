e8138b7fffa43a7c035093a1cc92b1f0a3a1cd9a
drocic <59014769+drocic@users.noreply.github.com>
2021-04-12 23:22:26 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
+}
+node 'localhost.localdomain' {
+  include role::master_server
 }
